24146bbc1bb5e4cd72627dfc39ce2110f203a8fb
Ricky Alejandro <586716+alexpineda@users.noreply.github.com>
2022-04-19 23:17:03 -0400

    remove splash screen

--- plugins/default-screens/index.jsx
+++ plugins/default-screens/index.jsx
@@ -38,36 +38,3 @@ registerComponent(
   { pluginId: "_plugin_id_", screen: "@map/loading" },
   ({ config }) => <LoadingScreen config={config} type="map" />
 );
-
-registerComponent(
-  { pluginId: "_plugin_id_", screen: "@home/ready" },
-  ({ config }) => {
-
-    return (
-      <div
-        style={{
-          display: config.splash ? "block" : "none",
-          position: "absolute",
-          zIndex: "-999",
-          left: "50%",
-          top: "50%",
-          transform: `translate(-50%, -50%)`,
-        }}
-      >
-          
-        <h1 style={{ fontFamily: "Conthrax", color: "var(--orange-5)", animation: "var(--animation-slide-in-down) forwards, var(--animation-fade-in)", animationDuration: "5s"  }}>
-          Titan Reactor
-        </h1>
-        <p
-          style={{
-            marginTop: "var(--size-2)",
-            textAlign: "center",
-            color: "var(--gray-4)",
-          }}
-        >
-          Menu: ALT, Fullscreen: F11, Plugins: F10
-        </p>
-      </div>
-    );
-  }
-);
