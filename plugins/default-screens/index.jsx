0a80961a34aa4a77caa3f29120045d12442b131f
Ricky Alejandro <586716+alexpineda@users.noreply.github.com>
2022-04-20 21:59:53 -0400

    production bar improvements

--- plugins/default-screens/index.jsx
+++ plugins/default-screens/index.jsx
@@ -1,5 +1,13 @@
-import { registerComponent, usePluginConfig, useReplay, useMap } from "titan-reactor";
+import {
+  registerComponent,
+  usePluginConfig,
+  useReplay,
+  useMap,
+  useStore
+} from "titan-reactor";
 import React from "react";
+
+const _updateAvailable = (store) => store.updateAvailable;
 
 const LoadingScreen = ({ type }) => {
   const config = usePluginConfig();
@@ -7,25 +15,27 @@ const LoadingScreen = ({ type }) => {
   const map = useMap();
 
   return (
-    <div  style={{
-      display: config[type] ? "block" : "none",
-      position: "absolute",
-      zIndex: "-999",
-      left: "50%",
-      top: "50%",
-      transform: `translate(-50%, -50%)`,
-    }}>
-    <h1
+    <div
       style={{
-        color: "white",
-        animation: "var(--animation-scale-up) forwards var(--ease-squish-2)"
+        display: config[type] ? "block" : "none",
+        position: "absolute",
+        zIndex: "-999",
+        left: "50%",
+        top: "50%",
+        transform: `translate(-50%, -50%)`,
       }}
     >
-      <p>{map?.title}</p>
-      {replay?.players.map((player) => (
-        <p key={player.id}>{player.name}</p>
-      ))}
-    </h1>
+      <h1
+        style={{
+          color: "white",
+          animation: "var(--animation-scale-up) forwards var(--ease-squish-2)",
+        }}
+      >
+        <p>{map?.title}</p>
+        {replay?.players.map((player) => (
+          <p key={player.id}>{player.name}</p>
+        ))}
+      </h1>
     </div>
   );
 };
@@ -39,3 +49,53 @@ registerComponent(
   { pluginId: "_plugin_id_", screen: "@map/loading" },
   ({ config }) => <LoadingScreen config={config} type="map" />
 );
+
+
+registerComponent({ pluginId: "_plugin_id_", screen: "@home/ready", snap: "center" }, () => {
+  const config = usePluginConfig();
+  const updateAvailable = useStore(_updateAvailable);
+
+  if (!config.homePage) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h1
+        style={{
+          fontFamily: "Conthrax",
+          color: "var(--orange-5)",
+          animation:
+            "var(--animation-slide-in-down) forwards, var(--animation-fade-in)",
+          animationDuration: "5s",
+        }}
+      >
+        Titan Reactor
+      </h1>
+      <p
+        style={{
+          marginTop: "var(--size-2)",
+          textAlign: "center",
+          color: "var(--gray-4)",
+        }}
+      >
+        Menu: ALT, Fullscreen: F11, Plugins: F10
+      </p>
+      {updateAvailable && (
+        <div
+          style={{
+            color: "var(--green-5)",
+            textAlign: "center",
+            textDecoration: "underline",
+            cursor: "pointer",
+          }}
+          onClick={() =>
+            window.parent.postMessage("system:download-update", "*")
+          }
+        >
+          Download New Version {updateAvailable.version} Now!
+        </div>
+      )}
+    </div>
+  );
+});
--- plugins/production-bar/index.jsx
+++ plugins/production-bar/index.jsx
@@ -4,7 +4,7 @@ import {
   usePlayers,
   useProduction,
   useStyleSheet,
-  usePluginConfig
+  usePluginConfig,
 } from "titan-reactor";
 import ProductionItem from "./production-item.jsx";
 import { useToggleItems } from "./toggle-items.js";
@@ -32,32 +32,67 @@ const ProductionBar = () => {
   );
 
   const filterUpgrade = (upgrade) => {
-    return config.showUpgrades && (upgrade.progress > 0 || config.showCompletedUpgrades && upgrade.progress === 0);
-  }
+    return (
+      config.showUpgrades &&
+      (upgrade.progress > 0 ||
+        (config.showCompletedUpgrades && upgrade.progress === 0))
+    );
+  };
 
   const filterResearch = (research) => {
-    return config.showResearch && (research.progress > 0 || config.showCompletedResearch && research.progress === 0);
-  }
+    return (
+      config.showResearch &&
+      (research.progress > 0 ||
+        (config.showCompletedResearch && research.progress === 0))
+    );
+  };
 
   if (config.toggleVisible === false) {
-      return null;
+    return null;
   }
 
-  return players.map((player) => {
-    const units = getUnits(player.id).slice(-config.maxUnits);
-    const upgrades = getUpgrades(player.id).filter(filterUpgrade);
-    const research = getResearch(player.id).filter(filterResearch);
-    // const items = useToggleItems(config.rotateDisplay, units, upgrades, research);
-    const items = [...units, ...upgrades, ...research];
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: config.orientation === "horizontal" ? "column" : "row",
+        padding: "var(--size-2)",
+        borderRadius: "var(--radius-2)",
+        backgroundColor: config.backgroundColor,
+      }}
+    >
+      {players.map((player) => {
+        const units = getUnits(player.id)
+          .slice(-config.maxUnits)
+          .sort((a, b) => a.count - b.count)
+          .sort((a, b) => a.typeId - b.typeId);
+        const tech = [...getUpgrades(player.id).filter(filterUpgrade), ...getResearch(player.id).filter(filterResearch)].sort((a, b) => b.progress - a.progress);
+        // const items = useToggleItems(config.rotateDisplay, units, upgrades, research);
+        const items = config.unitsFirst ? [...units, ...tech] : [...tech, ...units];
 
-    return (
-      <div style={{ display:"flex" , padding: "var(--size-1)", backgroundColor: config.backgroundColor }}>
-        {items.slice(0, config.maxTotalItems).map((item, i) => (
-          <ProductionItem item={item} color={player.color} isFirst={i === 0} />
-        ))}
-      </div>
-    );
-  });
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection:
+                config.orientation === "horizontal" ? "row" : "column",
+              padding: "var(--size-2)",
+              borderRadius: "var(--radius-2)",
+              backgroundColor: config.backgroundColor,
+            }}
+          >
+            {items.slice(0, config.maxTotalItems).map((item, i) => (
+              <ProductionItem
+                item={item}
+                color={player.color}
+                isFirst={i === 0}
+              />
+            ))}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 registerComponent(
