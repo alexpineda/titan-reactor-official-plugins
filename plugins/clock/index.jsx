e7388fe222146a2ec09853a4aa626782663b1cdd
Ricky Alejandro <586716+alexpineda@users.noreply.github.com>
2022-06-29 03:26:21 -0400

    fix color bug in clock

--- plugins/clock/index.jsx
+++ plugins/clock/index.jsx
@@ -7,7 +7,9 @@ import ClassicClock from "./classic-clock.jsx";
 registerComponent(
   { pluginId: "_plugin_id_", screen: "@replay/ready", snap: "left", order: -100 },
   () => {
+    // we can use the plugin config to determine which clock to show as well as colors and other settings
     const config = usePluginConfig();
+    // frame will be updated every game second with useful info like time and frame #.
     const frame = useFrame();
 
     const pct = `${Math.round((frame.frame / frame.maxFrame) * 100)}%`;
@@ -15,7 +17,7 @@ registerComponent(
     const styles = {
       timeLabel: "white",
       bevelGray800Reverse:
-        `linear-gradient(135deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 50%, ${config.bgDark} 50%, #2d3748 100%)`,
+        `linear-gradient(135deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 50%, ${config.bgDark} 50%, ${config.bgDark} 100%)`,
       bevelGray800:
         `linear-gradient(45deg, ${config.bgDark} 0%, ${config.bgDark} 50%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0) 100%)`,
       bgGray700: config.bgLight,
