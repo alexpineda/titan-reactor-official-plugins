578bd10ea84f91f28c4c2f86064e7951e43c024b
Ricky Alejandro <586716+alexpineda@users.noreply.github.com>
2022-06-29 01:11:38 -0400

    simplify player colors plugin

--- plugins/player-colors/plugin.js
+++ plugins/player-colors/plugin.js
@@ -1,29 +1,30 @@
-const { THREE } = arguments[0];
-
-const colors = [];
-
 return {
-    _updateColors(forceDisable) {
+    _updateColors() {
+      const colors = [];
+
+      // load a list of colors from the config player_0, player_1 etc.
       for (let i = 0; i < 8; i++) {
         colors[i] = this.config[`player_${i}`];
       }
 
-      if (this.config.enabled && !forceDisable) {
+      if (this.config.enabled) {
         this.setPlayerColors(colors);
       } else {
         this.setPlayerColors(this.getOriginalColors());
       }
     },
 
+    /*
+    * When the game is ready to start but before it has been drawn. 
+    */
     onGameReady() {
       this._updateColors();
     },
 
+    /*
+     * If this plugins config has changed, let's update any colors that we have set.
+     */
     onConfigChanged() {
       this._updateColors();
-    },
-
-    onPluginDispose() {
-      this._updateColors(true);
     }
 }
