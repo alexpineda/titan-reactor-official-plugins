1f0a900d63458d9b958f46ed9b21502b037170bf
Ricky Alejandro <586716+alexpineda@users.noreply.github.com>
2022-06-30 01:39:49 -0400

    Add randomize option allowing to use custom colors in a random order every game

--- plugins/player-colors/plugin.js
+++ plugins/player-colors/plugin.js
@@ -1,10 +1,21 @@
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+  return array;
+}
+
+const randomized = shuffle(new Array(8).fill(0).map((_, i) => i));
+
 return {
     _updateColors() {
       const colors = [];
 
       // load a list of colors from the config player_0, player_1 etc.
       for (let i = 0; i < 8; i++) {
-        colors[i] = this.config[`player_${i}`];
+        if (this.config.randomized) {
+          colors[i] = this.config[`player_${randomized[i]}`];
+        } else {
+          colors[i] = this.config[`player_${i}`];
+        }
       }
 
       if (this.config.enabled) {
